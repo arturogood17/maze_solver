accbcfd1b4ca2d85a82a830d9567ebda703e9872
arturogood17 <arturo_good17@hotmail.com>
2024-11-22 14:50:41 +0100

    Win check

--- cell.py
+++ cell.py
@@ -12,6 +12,8 @@ class Cell:
         self.__win = win
     
     def draw(self, x1, y1, x2, y2):
+        if self.__win is None:
+            return
         self.__x1= x1
         self.__y1= y1
         self.__x2= x2
@@ -28,3 +30,6 @@ class Cell:
         if self.has_bottom_wall:
             line= Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
             self.__win.draw_line(line)
+
+    def draw_move(self, to_cell, undo= False):
+
