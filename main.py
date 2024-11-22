555610ef99227acf3b99d6a72debe2ede222cc27
arturogood17 <arturo_good17@hotmail.com>
2024-11-22 14:47:25 +0100

    Cell class (to draw cells)

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from graphics import *
+from cell import Cell
 def main():
     win= Window(800, 600)
-    line= Line(Point(50, 50), Point(400, 400))
-    win.draw_line(line, "black")
+    celda = Cell(win)
+    celda.has_left_wall= False
+    celda.draw(50, 50, 100, 100)
+
+    celda= Cell(win)
+    celda.has_right_wall= False
+    celda.draw(150,150,200,200)
+
+    celda= Cell(win)
+    celda.has_top_wall= False
+    celda.draw(225,225,200,200)
+
+    celda= Cell(win)
+    celda.has_bottom_wall= False
+    celda.draw(300,300, 500,500)
+
+
     win.wait_for_close()
 
 main()
