5a0b2950178c1a3d039294080b6c22b711989d98
arturogood17 <arturo_good17@hotmail.com>
2024-11-23 13:29:09 +0100

    Testing connecting lines

--- main.py
+++ main.py
@@ -3,20 +3,28 @@ from cell import Cell
 def main():
     win= Window(800, 600)
     celda = Cell(win)
-    celda.has_left_wall= False
+    celda.has_right_wall= False
     celda.draw(50, 50, 100, 100)
 
-    celda= Cell(win)
-    celda.has_right_wall= False
-    celda.draw(150,150,200,200)
+    celda1= Cell(win)
+    celda1.has_left_wall= False
+    celda1.has_bottom_wall= False
+    celda1.draw(100,50,150,100)
 
-    celda= Cell(win)
-    celda.has_top_wall= False
-    celda.draw(225,225,200,200)
+    celda.draw_move(celda1)
 
-    celda= Cell(win)
-    celda.has_bottom_wall= False
-    celda.draw(300,300, 500,500)
+    celda2= Cell(win)
+    celda2.has_top_wall= False
+    celda2.has_right_wall= False
+    celda2.draw(100,100,150, 150)
+
+    celda1.draw_move(celda2)
+
+    celda3= Cell(win)
+    celda3.has_left_wall= False
+    celda3.draw(150,100, 200,150)
+
+    celda2.draw_move(celda3, True)
 
 
     win.wait_for_close()
