3b4e37e07be8e169eefee4c3df9af05285ac62da
arturogood17 <arturo_good17@hotmail.com>
2024-11-23 14:04:23 +0100

    Drawing a grid

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
 from graphics import *
 from cell import Cell
+from maze import Maze
 def main():
-    win= Window(800, 600)
-    celda = Cell(win)
-    celda.has_right_wall= False
-    celda.draw(50, 50, 100, 100)
+    num_rows= 12
+    num_cols= 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x= (screen_x - 2 * margin)/ num_cols
+    cell_size_y = (screen_y - 2 * margin)/ num_rows
+    win = Window(screen_x, screen_y)
 
-    celda1= Cell(win)
-    celda1.has_left_wall= False
-    celda1.has_bottom_wall= False
-    celda1.draw(100,50,150,100)
-
-    celda.draw_move(celda1)
-
-    celda2= Cell(win)
-    celda2.has_top_wall= False
-    celda2.has_right_wall= False
-    celda2.draw(100,100,150, 150)
-
-    celda1.draw_move(celda2)
-
-    celda3= Cell(win)
-    celda3.has_left_wall= False
-    celda3.draw(150,100, 200,150)
-
-    celda2.draw_move(celda3, True)
-
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
 
     win.wait_for_close()
 
